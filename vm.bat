397adb1d7f7d92183ea311032776390d1b70526a
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-14 21:47:25 +0100

    Added testsource to classpath in order to reference IML

--- vm.bat
+++ vm.bat
@@ -3,10 +3,10 @@ SETLOCAL EnableDelayedExpansion
 SETLOCAL EnableExtensions
 
 SET JAVA_CLASSPATH=
-SET JAVA_CLASSPATH=%JAVA_CLASSPATH%.\build\libs\*
+SET JAVA_CLASSPATH=%JAVA_CLASSPATH%.\build\libs\*;
+SET JAVA_CLASSPATH=%JAVA_CLASSPATH%.\src\test\resources\;
 SET FILE=Sample.iml
 
 
 SET EXECUTE=java  -Dfile=%FILE%  -cp %JAVA_CLASSPATH% ch.fhnw.lederer.virtualmachine.TestVM
-echo %EXECUTE%
 %EXECUTE%
