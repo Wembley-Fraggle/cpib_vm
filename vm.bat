9a25dfbc6a14dec1b3be5a1194c830abe24aa77e
Wembley-Fraggle <roman.holzner@students.fhwn.ch>
2015-01-14 21:30:52 +0100

    We can specify classpath as directory

--- vm.bat
+++ vm.bat
@@ -3,12 +3,10 @@ SETLOCAL EnableDelayedExpansion
 SETLOCAL EnableExtensions
 
 SET JAVA_CLASSPATH=
-SET JAVA_CLASSPATH=%JAVA_CLASSPATH%./build/libs/vm.jar;
-SET JAVA_CLASSPATH=%JAVA_CLASSPATH%lib\antlr4-runtime-4.3.jar;
-SET JAVA_CLASSPATH=%JAVA_CLASSPATH%lib\antlr4-annotations-4.3.jar
-SET JAVA_CLASSPATH=%JAVA_CLASSPATH%lib\org.abego.treelayout.core-1.0.1.jar
+SET JAVA_CLASSPATH=%JAVA_CLASSPATH%.\build\libs\*
 SET FILE=Sample.iml
 
 
-SET EXECUTE=java -Dfile=%FILE% -cp %JAVA_CLASSPATH% ch.fhnw.lederer.virtualmachine.TestVM 
+SET EXECUTE=java  -Dfile=%FILE%  -cp %JAVA_CLASSPATH% ch.fhnw.lederer.virtualmachine.TestVM
+echo %EXECUTE%
 %EXECUTE%
